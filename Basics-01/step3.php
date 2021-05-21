d7265f0cb741ca804a9ee4591d5a0e7c026c6f93
Ahmad_h97 <ahmadhamdach1997@gmail.com>
2021-05-21 14:07:32 +0300

    step3

---
+++ Basics-01/step3.php
@@ -0,0 +1,8 @@
+<?php
+$name = 'Ahmad';
+$email = 'ahmadhamdach1997@gmail.com';
+$height = 196;
+$width = 67;
+$area  = $height * $width;
+echo "My name is $name my email is $email and height is $height and  my width is $width my area is $area";
+?>
